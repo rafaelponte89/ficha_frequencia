c7bc581ad0c78b3bd00eb935da55b5bc9291624a
Rafael Ponte Cassemiro <rafael_ponte1b@hotmail.com>
2022-11-13 20:53:30 -0300

    Reorganização de parte do código A opção encerrar ano aparece na listagem das fichas, não mais na própria ficha Mudança em código redundante

--- app_ficha_cem/urls.py
+++ app_ficha_cem/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('pessoas/', pessoas, name="listarpessoas"),
     path('pessoas/<int:pessoa_id>', atualizar_pessoa, name="atualizarpessoa"),
     path('pessoas/<int:pessoa_id>/faltas', pessoas_faltas, name="lancarfalta" ),
-    path('pessoas/<int:pessoa_id>/fichas', listar_ficha, name='ficha' ),
+    path('pessoas/<int:pessoa_id>/fichas', listar_ficha, name='listarficha' ),
     path('pessoas/<int:pessoa_id>/fichas/<int:ano>', gerar_ficha, name='ficha'),
     path('faltas/', faltas, name="listarfaltas"),
-    path('encerrarano/<int:pessoa_id>&<int:ano>', encerrar_ano, name='encerrarano' ),
+    path('pessoas/<int:pessoa_id>/fichas/encerramento/<int:ano>', encerrar_ano, name='encerrarano' ),
    
 ]
--- app_ficha_cem/views.py
+++ app_ficha_cem/views.py
@@ -11,7 +11,9 @@ from django.template.loader import get_template
 from datetime import datetime, timedelta
 from django.http import HttpResponse
 
+# determina se o ano é bissexto
 def bissexto(ano):
+
     if ano % 400 == 0:
         return True
     else:
@@ -20,80 +22,7 @@ def bissexto(ano):
                 return False
             return True
 
-
-
-def faltas(request):
-    faltas = Faltas.objects.all()
-    if request.method == 'POST':
-        form = formularioTF(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listarfaltas')
-    else:
-        form = formularioTF()
-    return render(request,'template/cadastrar_tipo_falta.html',{'form':form, 'faltas':faltas})
-
-def pessoas_faltas(request, pessoa_id):
-
-    pessoa = Pessoas.objects.get(pk=pessoa_id)
-    pessoa_faltas = Faltas_Pessoas.objects.all()
-
-
-    if request.method == 'POST':
-        form = formularioLF(request.POST)
-    
-        if form.is_valid():
-            form.save()
-            return redirect('lancarfalta',pessoa_id)
-    else:
-        form = formularioLF(initial={'pessoa':pessoa})
-    return render(request,'template/lancar_falta.html', {'form':form, 'pessoa':pessoa})
-
-def atualizar_pessoa(request, pessoa_id):
-    pessoa = Pessoas.objects.get(pk=pessoa_id)
-   
-    if request.method == 'POST':
-        form = formularioPessoa(request.POST, instance=pessoa)
-        if form.is_valid():
-            form.save()
-            return redirect('listarpessoas')
-    else:
-        form = formularioPessoa(instance=pessoa)
-    return render(request,'template/cadastrar_pessoa.html',{'form':form,'pessoa':pessoa})
-
-
-def pessoas(request):
-    pessoas = Pessoas.objects.all()
-
-    if request.method == 'POST':
-        form = formularioPessoa(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listarpessoas')
-    else:
-
-        form = formularioPessoa()
-    return render(request,'template/cadastrar_pessoa.html',{'form':form, 'pessoas':pessoas})
-
-
-
-
-
-def listar_anos(pessoa_id):
-    anos = []
-    pessoa_faltas = Faltas_Pessoas.objects.all()
-    pessoa = Pessoas.objects.get(pk=pessoa_id)
-    for i in pessoa_faltas:
-        if i.data.year not in anos and i.pessoa.id == pessoa_id:
-            anos.append(i.data.year)
-    return anos, pessoa
-
-def listar_ficha(request, pessoa_id):
-
-    anos, pessoa = listar_anos(pessoa_id)
-
-    return render(request,'template/listar_ficha.html',{'anos':anos, 'pessoa':pessoa})
-
+# retorna a quantidade de dias de um determinado ano
 def retornar_dias(ano):
     dias    = 365
 
@@ -112,7 +41,6 @@ def configurar_meses(ano):
     if bissexto(ano):
         fevereiro.append('C')
        
-
     meses = {
         'janeiro': meses_31.copy(),
         'fevereiro': fevereiro,
@@ -130,6 +58,84 @@ def configurar_meses(ano):
 
     return meses
 
+# função recursiva que determina se a data é útil para o tipo P, senão retorna própria data
+def data_util(data, tp='P'):
+    
+    if tp == 'P':
+        if (data.weekday() != 6 and data.weekday() != 5):
+            return data
+        data = data + timedelta(days=1)
+        return data_util(data)
+    return data
+
+# fazer o lançamento de faltas para determinada pessoa
+def pessoas_faltas(request, pessoa_id):
+
+    pessoa = Pessoas.objects.get(pk=pessoa_id)
+
+    if request.method == 'POST':
+        form = formularioLF(request.POST)
+    
+        if form.is_valid():
+            form.save()
+            return redirect('lancarfalta',pessoa_id)
+    else:
+        form = formularioLF(initial={'pessoa':pessoa})
+    return render(request,'template/lancar_falta.html', {'form':form, 'pessoa':pessoa})
+
+# atualiza informações de uma pessoa
+def atualizar_pessoa(request, pessoa_id):
+    pessoa = Pessoas.objects.get(pk=pessoa_id)
+   
+    if request.method == 'POST':
+        form = formularioPessoa(request.POST, instance=pessoa)
+        if form.is_valid():
+            form.save()
+            return redirect('listarpessoas')
+    else:
+        form = formularioPessoa(instance=pessoa)
+    return render(request,'template/cadastrar_pessoa.html',{'form':form,'pessoa':pessoa})
+
+# listar e incluir pessoas
+def pessoas(request):
+    pessoas = Pessoas.objects.all()
+
+    if request.method == 'POST':
+        form = formularioPessoa(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarpessoas')
+    else:
+
+        form = formularioPessoa()
+    return render(request,'template/cadastrar_pessoa.html',{'form':form, 'pessoas':pessoas})
+
+# listar anos de uma determinada pessoa
+def listar_anos(pessoa_id):
+    anos = []
+    pessoa_faltas = Faltas_Pessoas.objects.all()
+    pessoa = Pessoas.objects.get(pk=pessoa_id)
+
+    for i in pessoa_faltas:
+        if i.data.year not in anos and i.pessoa.id == pessoa_id:
+            anos.append(i.data.year)
+
+    return anos, pessoa
+
+def listar_ficha(request, pessoa_id):
+    anos, pessoa = listar_anos(pessoa_id)
+    anos_status = {}
+    for ano in anos:
+        status  = checar_existencia_pontuacao(ano,pessoa)
+        if status:
+            status = 'Aberto'
+        else:
+            status = 'Fechado'
+        anos_status[ano] = status
+  
+    return render(request,'template/listar_ficha.html',{'anos':anos_status, 'pessoa':pessoa})
+
+# ainda não utilizada
 def faltas_a_descontar(ano,pessoa):
     # atribuição 
     maior = Faltas_Pessoas.objects.all().filter(data__gte=f'{ano-1}-11-01')
@@ -147,7 +153,7 @@ def faltas_a_descontar(ano,pessoa):
     return n_faltas
 
 
-def render_to_pdf(template_src, context_dict={}):
+# def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     
     html = template.render(context_dict)
@@ -158,22 +164,12 @@ def render_to_pdf(template_src, context_dict={}):
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
 
-def gerar_ficha(request, pessoa_id, ano, pdf=None):
-    
-    pessoa = Pessoas.objects.get(pk=pessoa_id)
-    meses = configurar_meses(ano)
-    cargo, funcao, ue, pontuacao  = recuperar_pontuacao_anual(ano,pessoa)
-    cargo_a, funcao_a, ue_a  = gerar_pontuacao_anual(ano,pessoa,'a')
-    dias = range(1,32)
-    tipo_faltas = {}
-    faltas = Faltas_Pessoas.objects.all().order_by('data').filter(data__year=ano).filter(pessoa=pessoa_id)
-    
-    # for d in faltas:
-    #     print(d.falta.tipo, d.qtd_dias, d.data)
-
+# conta os tipos de faltas construindo um dicionário
+def contar_tipos_faltas(faltas):
 
     # insere no dicionario nova falta e atualiza quantidade da falta
     qtd = 0
+    tipo_faltas ={}
     for f in faltas:
         sigla = f.falta.tipo
         descricao = f.falta.descricao
@@ -186,6 +182,34 @@ def gerar_ficha(request, pessoa_id, ano, pdf=None):
             qtd += f.qtd_dias
             tipo_faltas[sigla][1] = qtd
 
+    return tipo_faltas
+
+# listar e incluir faltas
+def faltas(request):
+    faltas = Faltas.objects.all()
+    if request.method == 'POST':
+        form = formularioTF(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarfaltas')
+    else:
+        form = formularioTF()
+    return render(request,'template/cadastrar_tipo_falta.html',{'form':form, 'faltas':faltas})
+
+
+def gerar_ficha(request, pessoa_id, ano, pdf=None):
+    
+    pessoa = Pessoas.objects.get(pk=pessoa_id)
+    meses = configurar_meses(ano)
+    
+    cargo, funcao, ue  = gerar_pontuacao_anual(ano,pessoa)
+    cargo_a, funcao_a, ue_a  = gerar_pontuacao_anual(ano,pessoa,'a')
+    dias = range(1,32)
+    faltas = Faltas_Pessoas.objects.all().order_by('data').filter(data__year=ano).filter(pessoa=pessoa_id)
+
+
+    tipo_faltas=contar_tipos_faltas(faltas)
+
     data = ''
     for falta in faltas:
        
@@ -195,8 +219,8 @@ def gerar_ficha(request, pessoa_id, ano, pdf=None):
               
             if d > 0:
                 data = data + timedelta(days=1)
-            
-            # retorna as data úteis se tipo da falta for P, caso contrário retorna a própria data
+
+            # retorna as datas úteis se tipo da falta for P, caso contrário retorna a própria data
             data = data_util(data, falta.falta.tipo)
 
             mes = data.month
@@ -227,7 +251,6 @@ def gerar_ficha(request, pessoa_id, ano, pdf=None):
             elif mes == 12:
                 meses['dezembro'][dia] = falta.falta.tipo
     
-    
     contexto = {
         'meses': meses,
         'ano': ano,
@@ -239,86 +262,74 @@ def gerar_ficha(request, pessoa_id, ano, pdf=None):
         'ue_a': ue_a,
         'nome': pessoa.nome,
         'pessoa': pessoa,
-        'pontuacao': pontuacao,
+    
         'dias': dias,
         'tp_faltas': tipo_faltas,
+        'status': 'aberto'
         # 'pagesize':'A4'
 
     }
 
-    if pdf != None: 
-        pdf = render_to_pdf('template/ficha_cem.html',contexto)
-        return HttpResponse(pdf, content_type='application/pdf')
+    # if pdf != None: 
+    #     pdf = render_to_pdf('template/ficha_cem.html',contexto)
+    #     return HttpResponse(pdf, content_type='application/pdf')
         
     return render(request,'template/ficha_cem.html', {'contexto':contexto})
 
-# função recursiva que determina se a data é útil para o tipo P, senão retorna própria data
-def data_util(data, tp='P'):
-    
-    if tp == 'P':
-        if (data.weekday() != 6 and data.weekday() != 5):
-            return data
-        data = data + timedelta(days=1)
-        return data_util(data)
-    return data
-   
-   
-   
 def index(request):
     return render(request,'template/index.html')
 
 def encerrar_ano(request, pessoa_id, ano):
-    status = f'Ano {ano} não Encerrado! Existem anos anteriores em aberto!'
+
     pessoa = Pessoas.objects.get(pk=pessoa_id)
     cargo, funcao, ue =   gerar_pontuacao_anual(ano,pessoa)
     cargo_a, funcao_a, ue_a =   gerar_pontuacao_anual(ano,pessoa,'a')
     soma_a = cargo_a + funcao_a + ue_a
     anos, pessoa = listar_anos(pessoa.id)
-
-    
-    
-    if anos.index(ano) == 0 and soma_a == 0:
-        pontuacao = Pontuacoes(ano=ano,cargo=cargo,funcao=funcao,ue=ue,pessoa=pessoa)
-        pontuacao.save()
-        status = f'Ano {ano} Encerrado!'
-    else: 
-        if soma_a != 0:
+    mensagem =f'Não foi possível encerrar o ano {ano}!'
+   
+    try:
+        if anos.index(ano) == 0 and soma_a == 0:
             pontuacao = Pontuacoes(ano=ano,cargo=cargo,funcao=funcao,ue=ue,pessoa=pessoa)
             pontuacao.save()
-            status = f'Ano {ano} Encerrado!'
+            mensagem = f'Ano {ano} foi encerrado com sucesso!'
+        else: 
+            if soma_a != 0:
+                pontuacao = Pontuacoes(ano=ano,cargo=cargo,funcao=funcao,ue=ue,pessoa=pessoa)
+                pontuacao.save()
+                mensagem = f'Ano {ano} foi encerrado com sucesso!'   
+    except:
+        return render(request, 'template/encerrar_ano.html', {'mensagem':mensagem, 'pessoa':pessoa})
 
-    contexto = {
-        'ano': ano,
-        'pessoa':pessoa,
-        'status': status
-    }
-    
-    
-    return render(request, 'template/encerrar_ano.html', {'contexto': contexto})
+            
+    return render(request, 'template/encerrar_ano.html', {'mensagem':mensagem,'pessoa':pessoa})
 
-def recuperar_pontuacao_anual(ano, pessoa):
+    
+# recupera a pontuação do ano corrente 
+def checar_existencia_pontuacao(ano, pessoa):
     status = True
     q2 = Pontuacoes.objects.filter(pessoa=pessoa)
     q3 = Pontuacoes.objects.filter(ano=ano)
     pontuacao = q2.intersection(q3)
 
     if len(pontuacao):
-        cargo, funcao, ue = pontuacao[0].cargo, pontuacao[0].funcao, pontuacao[0].ue
         status = False
     else:
-        cargo, funcao, ue = 0,0,0
+        status = True
 
-    return cargo, funcao, ue, status 
+    return  status 
 
-def gerar_pontuacao_anual(ano,pessoa, tipo='n'):
+def gerar_pontuacao_anual(ano,pessoa, tipo='c'):
+    '''a - ano anterior, c - ano corrente'''
 
-   
-    dias = retornar_dias(ano)
-    q1 = Pontuacoes.objects.filter(ano=ano-1)
+    dias = retornar_dias(ano) # ano corrente
+
+    q1 = Pontuacoes.objects.filter(ano=ano-1) # ano anterior
     q2 = Pontuacoes.objects.filter(pessoa=pessoa)
+
     pontuacao_anterior = q1.intersection(q2)
     
-    
+    # ano anterior
     if tipo == 'a':
         if len(pontuacao_anterior) == 0:
             cargo = 0
@@ -327,17 +338,18 @@ def gerar_pontuacao_anual(ano,pessoa, tipo='n'):
            
         else:
             cargo = pontuacao_anterior[0].cargo
-            funcao = pontuacao_anterior[0].cargo
-            ue = pontuacao_anterior[0].cargo
+            funcao = pontuacao_anterior[0].funcao
+            ue = pontuacao_anterior[0].ue
             
     else:
+        # ano corrente
         if len(pontuacao_anterior) == 0 :
             cargo = dias
             funcao = dias
             ue = dias
-        if len(pontuacao_anterior) > 0:
-             cargo = int(pontuacao_anterior[0].cargo) + dias
-             funcao = int(pontuacao_anterior[0].funcao) + dias
-             ue = int(pontuacao_anterior[0].ue) + dias
+        else:
+            cargo = int(pontuacao_anterior[0].cargo) + dias
+            funcao = int(pontuacao_anterior[0].funcao) + dias
+            ue = int(pontuacao_anterior[0].ue) + dias
              
     return cargo, funcao, ue
